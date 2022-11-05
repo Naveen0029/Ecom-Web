7c0504fa7a429884ca68b1dca91411c47af776f6
Naveen <67317410+Naveen0029@users.noreply.github.com>
2022-11-06 02:01:44 +0530

    Update config.js

--- db/ecom-proj/src/config.js
+++ db/ecom-proj/src/config.js
@@ -1,7 +1,7 @@
 const apiUrl =
   process.env.REACT_APP_ENDPORT === "development"
     ? "http://localhost:3030"
-    :"https://mighty-taiga-97720.herokuapp.com/";
+    :"https://mighty-taiga-97720.herokuapp.com";
     
 
 export default apiUrl;
